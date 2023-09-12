24ad135edff164fe2d8ad0a0ee8e6cb376a1b466
Gustavo Sousa <gfsousa42@gmail.com>
2023-09-12 09:14:33 -0300

    nginx on

--- srcs/requirements/nginx/tools/setup.sh
+++ srcs/requirements/nginx/tools/setup.sh
@@ -4,9 +4,10 @@ CERT_PATH="/etc/ssl/certs/nginx-selfsigned.crt"
 KEY_PATH="/etc/ssl/private/nginx-selfsigned.key"
 
 # Generate SSL/TLS certificate and key using OpenSSL
-openssl req -new -newkey rsa:2048 
-            -days 365 
-            -nodes -x509 -keyout "$KEY_PATH" -out "$CERT_PATH" 
-            -subj "/C=US/ST=State/L=City/O=Organization/CN=gusousa.42.fr"
+openssl req -new -newkey rsa:2048 \
+            -days 365 \
+            -nodes -x509 -keyout "$KEY_PATH" -out "$CERT_PATH" \
+            -subj "/C=US/ST=State/L=City/ O=Organization/CN=gusousa.42.fr" \
+            -passout pass:"123"
 
 exec nginx
