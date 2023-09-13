714d2ca0909bba0b26047068a9dbae00f3ec4051
Gustavo Sousa <gfsousa42@gmail.com>
2023-09-13 12:28:16 -0300

    setup for mariaDB

--- srcs/requirements/nginx/tools/setup.sh
+++ srcs/requirements/nginx/tools/setup.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-CERT_PATH="/etc/ssl/certs/nginx-selfsigned.crt"
-KEY_PATH="/etc/ssl/private/nginx-selfsigned.key"
 
 # Generate SSL/TLS certificate and key using OpenSSL
 openssl req -new -newkey rsa:2048 \
