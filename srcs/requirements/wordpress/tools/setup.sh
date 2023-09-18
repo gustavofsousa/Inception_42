fc964fc709f8a816e766de380eb9cef821e35377
Gustavo Sousa <gfsousa42@gmail.com>
2023-09-18 18:50:54 -0300

    continuing fix: corre alpine if worked

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -1,45 +1,31 @@
 #!/bin/sh
 
-echo "check if it already exist"
-if [ ! -f "$WORDPRESS_DIR/wp-config.php" ]; then
-    # Download and extract WordPress
-    wp core download --path="$WORDPRESS_DIR"
-    echo "ready to install"
-    if [ $? -ne 0 ]; then
-        echo "WordPress download failed."
-        exit 1
-    fi
+rm -rf /www/wordpress
 
-    # Change directory to WordPress installation
-    cd "$WORDPRESS_DIR"
-    echo "switch to $WORDPRESS_DIR"
+echo "check if is already"
+if [ ! -f "/www/wordpress/wp-config.php" ]
+then
+    echo "I will install"
+	wp core download --path=/www/wordpress/
+	if [ $? != 0 ]
+	then
+		rmdir /www/wordpress
+		exit 0
+	fi
+	cd www/wordpress
+    echo "Ready to config wp"
+	mv wp-config-sample.php wp-config.php
+	sed -i "s/database_name_here/$DB_NAME/g" wp-config.php
+	sed -i "s/username_here/$DB_USER/g" wp-config.php
+	sed -i "s/password_here/$DB_PASSWORD/g" wp-config.php
+	sed -i "s/localhost/$DB_HOST:3306/g" wp-config.php
+	sed -i "s-http://example.com-$DOMAIN_NAME-g" wp-config.php
 
-
-    # Configure wp-config.php
-    mv wp-config-sample.php wp-config.php
-    sed -i "s/database_name_here/$DB_NAME/g" wp-config.php
-    sed -i "s/username_here/$DB_USER/g" wp-config.php
-    sed -i "s/password_here/$DB_PASSWORD/g" wp-config.php
-    sed -i "s/localhost/$DB_HOST/g" wp-config.php
-
-    # Install WordPress
-    sudo -u www-data -i -- wp core install --url="$DOMAIN_NAME" --title="$SITE_TITLE" \
-        --admin_user="$ADMIN_USER" --admin_password="$ADMIN_PASSWORD" \
-        --admin_email="$ADMIN_EMAIL" --skip-email
-
-    # Create WordPress users
-    sudo -u www-data -i -- wp user create "$EDITOR_USER" "$EDITOR_EMAIL" --role='editor' --user_pass="$EDITOR_PASSWORD"
-    sudo -u www-data -i -- wp user create "$SUBSCRIBER_USER" "$SUBSCRIBER_EMAIL" --role='subscriber' --user_pass="$SUBSCRIBER_PASSWORD"
-
-    echo "all set done in wordpress"
+	wp core install --url=$DOMAIN_NAME --title=$SITE_TITLE \
+		--admin_user=$ADMIN_USER --admin_password=$ADMIN_PASSWORD \
+		--admin_email=$ADMIN_EMAIL --skip-email
+	wp user create $EDITOR_USER $EDITOR_EMAIL --user_pass=$EDITOR_PASSWORD
 fi
 
 echo "Leaving"
-
-# Set appropriate permissions
-chmod -R 755 "$WORDPRESS_DIR"
-mkdir -p /run/php/
-chown www-data:www-data /run/php/
-
-# Start PHP-FPM
-exec php-fpm7.4 -F
+exec php-fpm7 -F
