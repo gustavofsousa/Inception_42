f8b1291df7dace1136bf14c19b0b7df6009fb13e
Gustavo Sousa <gfsousa42@gmail.com>
2023-09-20 12:23:30 -0300

    WIP: connection wpp and db

--- srcs/requirements/nginx/tools/setup.sh
+++ srcs/requirements/nginx/tools/setup.sh
@@ -6,5 +6,5 @@ mkdir -p "/etc/ssl/private/"
 	openssl genrsa -out $KEY_PATH 2048
 	openssl req -new -x509 -key $KEY_PATH -out $CERT_PATH -days 365 -subj "/CN=$DOMAIN_NAME"
 
-
+echo "SSL generated, executing nginx now"
 exec nginx
